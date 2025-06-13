697cfe718044421967ac57d6ccba6c7104f41898
Drew Harris <drew@drewh.net>
2025-06-13 11:54:48 -0700

    successful shadcn install

--- src/ui/SpecificActionResult.tsx
+++ src/ui/SpecificActionResult.tsx
@@ -1,10 +1,29 @@
 import { useEffect, useState } from "react";
 import type { Action } from "../schemas/attempts";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const SpecificActionView = ({ action }: { action: Action }) => {
   const [previewType, setPreviewType] = useState<"html" | "image">("html");
   return (
-    <div className="bg-red-500 py-2">
+    <div className="py-2">
+      <div className="px-8 py-8">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
       <div>{action.id}</div>
       <div>{action.beforeSnapshot}</div>
       {previewType === "html" ? (
